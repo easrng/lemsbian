5778b7156018c30a2ddc3b93247214f9f4a0e763
easrng <easrng@gmail.com>
2021-11-15 13:10:25 -0500

    Update view.js

--- view.js
+++ view.js
@@ -22,8 +22,9 @@ function h(type, props, ...children) {
 const html = htm.bind(h);
 let next = "https://t.me/s/tilliegaystuff", scrollTo;
 try{
-  scrollTo=parseInt(location.hash.match(/\#post-(\d+)/)[1])
-  next="https://lesmbian.easrng.workers.dev/?before="+(scrollTo-(scrollTo%20)+20);
+  let m=location.hash.match(/\#post-(\d+)-(\d)/);
+  scrollTo={post:parseInt(m[1]), offset: parseInt(m[2])};
+  next="https://lesmbian.easrng.workers.dev/?before="+(scrollTo.post-(scrollTo.post%20)+20);
 }catch(e){}
 console.log("init");
 let loading=false;
@@ -38,19 +39,23 @@ async function load() {
       )).json(),
       "text/html"
     );
-    let images = Array.from(d.querySelectorAll(".tgme_widget_message")).map(m=>Array.from(m.querySelectorAll(".tgme_widget_message_photo_wrap")).map(e=>({src:e.style.backgroundImage.match(/url\(['"](.+)['"]\)/)[1],id:m.dataset.post.split("/")[1], caption: m.querySelector(".tgme_widget_message_text")?.innerText.trim()}))).flat();
+    let images = Array.from(d.querySelectorAll(".tgme_widget_message")).map(m=>Array.from(m.querySelectorAll(".tgme_widget_message_photo_wrap")).map((e, i)=>({src:e.style.backgroundImage.match(/url\(['"](.+)['"]\)/)[1],id:"post-"+m.dataset.post.split("/")[1]+"-"+i, caption: m.querySelector(".tgme_widget_message_text")?.innerText.trim()}))).flat();
     next = d.querySelector(".tme_messages_more").href;
     swiper.appendSlide(
       images.map(
         i => html`
-          <div className="swiper-slide" id=${"post-"+i.id}><img src=${i.src} />${i.caption?html`<div className="caption">${i.caption}</div>`:""}</div>
+          <div className=${"swiper-slide "+i.id}><img src=${i.src} />${i.caption?html`<div className="caption">${i.caption}</div>`:""}</div>
         `
       )
     );
     }catch(e){}
     loading=false;
+    if(scrollTo) {
+      swiper.slideTo(swiper.slides.indexOf(document.querySelector("#post-"+scrollTo.post))+scrollTo.offset)
+      scrollTo=null;
+    }
   }
 }
 swiper.on("slideChange", load);
-swiper.on("slideChange", ()=>history.replaceState(document.title,null,"#"+encodeURIComponent(swiper.slides[swiper.activeIndex].id)));
+swiper.on("slideChange", ()=>history.replaceState(document.title,null,"#"+encodeURIComponent(Array.from(swiper.slides[swiper.activeIndex].classList).filter(e=>e.startsWith("post-"))[0]));
 load();
